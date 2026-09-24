9c6f15db85c9993dc2ebf13b6f8af2e2fb034298
PhongTran99 <26730054@ms.uit.edu.vn>
2026-09-24 21:57:23 +0700

    feature/grid-blocks-ui
    - Create new UI for blocks using special character ■

--- main.cpp
+++ main.cpp
@@ -80,7 +80,7 @@ void draw(){
     for (int i = 0 ; i < H ; i++) {
         for (int j = 0 ; j < W ; j++) {
             if (board[i][j] == '#') cout << "▓▓";       // Vẽ viền
-            else if (board[i][j] != ' ') cout << "[]";  // Vẽ block
+            else if (board[i][j] != ' ') cout << "■ ";  // Vẽ block
             else cout << "  ";                        // Vẽ khoảng trống
         }
         cout << endl;
